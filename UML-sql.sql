9506e392016f4f3d90219609598535d593590089
Troy Campbell <tcampbel@ucsd.edu>
2015-03-30 16:40:56 +0200

    --> VARCHAR2

--- UML-sql.sql
+++ UML-sql.sql
@@ -3,20 +3,20 @@
 CREATE TABLE Categorie
 (
 IDCategorie INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-`Nom catégorie` CHAR NOT NULL,
+`Nom catégorie` VARCHAR2 NOT NULL,
 PRIMARY KEY (IDCategorie)
 );
 
 CREATE TABLE Client
 (
 IDClient INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-Nom CHAR NOT NULL,
-Prenom CHAR NOT NULL,
-VIlle CHAR NOT NULL,
-Adresse CHAR NOT NULL,
-Telephone CHAR,
-Email CHAR NOT NULL,
-`Mot de passe` CHAR NOT NULL,
+Nom VARCHAR2 NOT NULL,
+Prenom VARCHAR2 NOT NULL,
+VIlle VARCHAR2 NOT NULL,
+Adresse VARCHAR2 NOT NULL,
+Telephone VARCHAR2,
+Email VARCHAR2 NOT NULL,
+`Mot de passe` VARCHAR2 NOT NULL,
 `Date dinscription` DATE NOT NULL,
 NBCommande INTEGER NOT NULL,
 PRIMARY KEY (IDClient)
@@ -37,9 +37,9 @@ FOREIGN KEY (IDRemise) REFERENCES Remise(IDRemise)
 CREATE TABLE Entreprise
 (
 IDEntreprise INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-`Nom Entreprise` CHAR NOT NULL,
-Rue CHAR NOT NULL,
-Ville CHAR NOT NULL,
+`Nom Entreprise` VARCHAR2 NOT NULL,
+Rue VARCHAR2 NOT NULL,
+Ville VARCHAR2 NOT NULL,
 IDProduit INTEGER,
 IDCategorie INTEGER,
 PRIMARY KEY (IDEntreprise)
@@ -47,10 +47,11 @@ PRIMARY KEY (IDEntreprise)
 
 CREATE TABLE Livraison
 (
+IDLivraison INTEGER NOT NULL AUTO_INCREMENT UNIQUE, 
 IDEntreprise INTEGER NOT NULL,
 IDProduit INTEGER NOT NULL,
-IDcategorie INTEGER NOT NULL,
-Quantité INTEGER NOT NULL, 
+IDCategorie INTEGER NOT NULL,
+Quantite UNSIGNED-INTEGER NOT NULL, 
 `Date livraison` DATE NOT NULL,
 PRIMARY KEY (IDentreprise,IDProduit,IDcategorie,`Date livraison`)
 );
@@ -61,7 +62,7 @@ IDProduit INTEGER NOT NULL,
 IDCategorie INTEGER NOT NULL,
 IDEntreprise INTEGER NOT NULL,
 `Mois de production` DATE NOT NULL,
-`Lieu de production` CHAR NOT NULL,
+`Lieu de production` VARCHAR2 NOT NULL,
 PRIMARY KEY (IDproduit,IDCategorie,IDEntreprise)
 );
 
@@ -69,8 +70,8 @@ CREATE TABLE Produit
 (
 IDProduit INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
 IDCategorie INTEGER,
-`Nom produit` CHAR NOT NULL,
-Quantité INTEGER,
+`Nom produit` VARCHAR2 NOT NULL,
+Quantite INTEGER,
 `Date dajout` DATE,
 `Date de péremption` DATE,
 `Prix unitaire` INTEGER,
@@ -82,19 +83,18 @@ CREATE TABLE Panier
 IDPanier INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
 IDProduit INTEGER,
 IDCategorie INTEGER,
-Quantité INTEGER,
+Quantite INTEGER,
 PRIMARY KEY (IDPanier)
 );
 
 CREATE TABLE Remise
 (
 IDRemise INTEGER NOT NULL UNIQUE,
-`Type remise` CHAR NOT NULL,
+`Type remise` VARCHAR2 NOT NULL,
 Remise INTEGER,
 `Condition nb commande` INTEGER NOT NULL,
 PRIMARY KEY (IDRemise)
 );
-
 
 ALTER TABLE Commande ADD FOREIGN KEY IDClinent_idxfk (IDClinent) REFERENCES Client (IDClient);
 
