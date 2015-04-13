1e11fae45a5d2cb40c236b33360b1620cd3df45b
Bérénice Faltrept <berenice.faltrept@gmail.com>
2015-04-13 17:03:44 +0200

    rapport : tableau corrigé+ présentation // .sql : correction. Ca passe mais pas toutes FK

--- UML-BIS-sql.sql
+++ UML-BIS-sql.sql
@@ -2,17 +2,17 @@
 
 CREATE TABLE Admin
 (
-IDAdmin VARCHAR(255) NOT NULL UNIQUE,
+IDAdmin VARCHAR(40) NOT NULL UNIQUE,
 MotDePasse INTEGER NOT NULL,
 PRIMARY KEY (IDAdmin)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Categorie
 (
 IDCategorie INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-NomCatégorie VARCHAR2 NOT NULL,
+NomCatégorie VARCHAR(40) NOT NULL,
 PRIMARY KEY (IDCategorie)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Livraison
 (
@@ -22,19 +22,19 @@ IDEntreprise INTEGER NOT NULL,
 FraisDePort INTEGER NOT NULL,
 PrixTotalHT INTEGER,
 PRIMARY KEY (IDLivraison)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Produit
 (
 IDProduit INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
 IDCategorie INTEGER,
-NomProduit VARCHAR2 NOT NULL,
+NomProduit VARCHAR(70) NOT NULL,
 DateAjout DATE,
 PrixUnitaireHTVente INTEGER,
 DateDebutDisponibilite DATE,
 DateFinDisponibilite DATE,
 PRIMARY KEY (IDProduit,IDCategorie)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Production
 (
@@ -42,7 +42,7 @@ IDEntreprise INTEGER NOT NULL,
 IDProduit INTEGER NOT NULL,
 IDCategorie INTEGER NOT NULL,
 PRIMARY KEY (IDEntreprise,IDProduit,IDCategorie)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ProduitParCommande
 (
@@ -51,7 +51,7 @@ IDProduit INTEGER,
 IDCategorie INTEGER,
 Quantite INTEGER,
 PRIMARY KEY (IDCommande,IDProduit,IDCategorie)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ProduitParLivraison
 (
@@ -61,18 +61,18 @@ IDCategorie INTEGER,
 Quantite INTEGER,
 PrixUnitaireHTAchat INTEGER,
 PRIMARY KEY (IDLivraison)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Remise
 (
 IDRemise INTEGER NOT NULL UNIQUE,
-TypeRemise VARCHAR2 NOT NULL,
+TypeRemise ENUM('pourcent','fixe'),
 ValeurRemise INTEGER,
 ConditionNbCommande INTEGER,
 ConditionDuree INTEGER,
 ConditionDate Date,
 PRIMARY KEY (IDRemise)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Commande
 (
@@ -84,7 +84,7 @@ Valide INTEGER,
 PrixHT INTEGER,
 FraisDePort INTEGER,
 PRIMARY KEY (IDCommande)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Stock
 (
@@ -94,43 +94,43 @@ IDCategorie INTEGER NOT NULL UNIQUE,
 Quantite INTEGER,
 DatePeremption DATE,
 PRIMARY KEY (IDLivraison,IDPoduit,IDCategorie)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Ville
 (
 CodePostal INTEGER NOT NULL,
-NomVille VARCHAR2 NOT NULL,
+NomVille VARCHAR(100) NOT NULL,
 Livrable INTEGER NOT NULL,
 PRIMARY KEY (CodePostal,NomVille)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Entreprise
 (
 IDEntreprise INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-NomEntreprise VARCHAR2 NOT NULL,
-Rue VARCHAR2 NOT NULL,
-Ville CHAR NOT NULL,
+NomEntreprise VARCHAR(100) NOT NULL,
+Rue VARCHAR(100) NOT NULL,
+Ville VARCHAR(100) NOT NULL,
 CodePostal INTEGER NOT NULL,
 NomContact INTEGER,
 Contact INTEGER,
 PRIMARY KEY (IDEntreprise)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Client
 (
 IDClient INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-Nom VARCHAR2 NOT NULL,
-Prenom VARCHAR2 NOT NULL,
-Rue VARCHAR2 NOT NULL,
+Nom VARCHAR(100) NOT NULL,
+Prenom VARCHAR(100) NOT NULL,
+Rue VARCHAR(100) NOT NULL,
 CodePostal INTEGER NOT NULL,
-Ville CHAR,
-Telephone VARCHAR2,
-Email VARCHAR2 NOT NULL,
-MotDePasse VARCHAR2 NOT NULL,
+Ville VARCHAR(100),
+Telephone INTEGER,
+Email VARCHAR(100) NOT NULL,
+MotDePasse VARCHAR(100) NOT NULL,
 DateInscription DATE NOT NULL,
 NBCommande INTEGER NOT NULL,
 PRIMARY KEY (IDClient)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE Livraison ADD FOREIGN KEY IDEntreprise_idxfk (IDEntreprise) REFERENCES Entreprise (IDEntreprise);
 
@@ -140,9 +140,9 @@ ALTER TABLE Production ADD FOREIGN KEY IDEntreprise_idxfk_1 (IDEntreprise) REFER
 
 ALTER TABLE Production ADD FOREIGN KEY IDProduit_idxfk (IDProduit,IDCategorie) REFERENCES Produit (IDProduit,IDCategorie);
 
-ALTER TABLE ProduitParCommandes ADD FOREIGN KEY IDCommande_idxfk (IDCommande) REFERENCES Commande (IDCommande);
+ALTER TABLE ProduitParCommande ADD FOREIGN KEY IDCommande_idxfk (IDCommande) REFERENCES Commande (IDCommande);
 
-ALTER TABLE ProduitParCommandes ADD FOREIGN KEY IDProduit_idxfk_1 (IDProduit,IDCategorie) REFERENCES Produit (IDProduit,IDCategorie);
+ALTER TABLE ProduitParCommande ADD FOREIGN KEY IDProduit_idxfk_1 (IDProduit,IDCategorie) REFERENCES Produit (IDProduit,IDCategorie);
 
 ALTER TABLE ProduitParLivraison ADD FOREIGN KEY IDLivraison_idxfk (IDLivraison) REFERENCES Livraison (IDLivraison);
 
@@ -156,6 +156,6 @@ ALTER TABLE Stock ADD FOREIGN KEY IDLivraison_idxfk_1 (IDLivraison) REFERENCES L
 
 ALTER TABLE Stock ADD FOREIGN KEY IDPoduit_idxfk (IDPoduit,IDCategorie) REFERENCES Produit (IDProduit,IDCategorie);
 
-ALTER TABLE Entreprise ADD FOREIGN KEY Code Postal_idxfk (CodePostal,Ville) REFERENCES Ville (CodePostal,Ville);
+ALTER TABLE Entreprise ADD FOREIGN KEY CodePostal_idxfk (CodePostal,Ville) REFERENCES Ville (CodePostal,Ville);
 
 ALTER TABLE Client ADD FOREIGN KEY Ville_idxfk (Ville,CodePostal) REFERENCES Ville (Ville,CodePostal);
