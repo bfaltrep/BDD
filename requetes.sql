9b357348a852b845a713bca089561016a2cfa7fe
Bérénice Faltrept <berenice.faltrept@gmail.com>
2015-04-24 12:02:47 +0200

    insert

--- requetes.sql
+++ requetes.sql
@@ -1,4 +1,20 @@
-﻿
+﻿/* un lot pose probleme, nous voulons contacter toutes les personnes qui ont été livrés de ce produit depuis la livraison de ce lot.*/
+
+SELECT Cli_Email, Cli_Prenom, CLi_Nom FROM Client NATURAL JOIN (
+SELECT IDClient FROM Client NATURAL JOIN Commande WHERE Com_DateLivraison >= (
+SELECT LF_DateArrivee FROM LivraisonFournisseur NATURAL JOIN ProduitParLivraison WHERE IDProduit IN (
+SELECT IDProduit FROM Produit WHERE Pro_Nom = "congue" ) AND IDEntreprise IN (SELECT IDEntreprise FROM Entreprise WHERE Ent_Nom = "Flashset"))) as t;
+
+/**
+ * Dans le cas où quelqu’un est malade à cause de nos produit, on veut savoir quels produits cette personne a acheté sur une date précise.
+ * Exemple: jstewartp@un.org nous a écrit pour nous dire qu'il a eu mal au ventre après avoir mangé des produits de la commande qu'il a reçu le 7 Mai 2014.  
+ */
+SELECT * FROM Produit WHERE IDProduit IN (
+SELECT IDProduit FROM ProduitParCommande WHERE IDCommande LIKE (
+SELECT IDCommande FROM Commande WHERE Commande.IDClient LIKE (
+SELECT IDClient FROM Client WHERE Cli_Email = 'nlane7@digg.com') AND Com_DateLivraison='2014-11-09'));
+
+
 /*
 Produit périmés encore en stock pour les retirer de notre réserve.
 */
@@ -18,14 +34,8 @@ Je cherche quel producteurs va me fournir avec les prix les plus intéressant.
 
 SELECT Ent_Nom, Ent_NomContact, Ent_Contact, PPL_PrixUnitaireHTAchat 
 FROM ProduitParLivraison NATURAL JOIN  LivraisonFournisseur NATURAL JOIN Entreprise NATURAL JOIN Produit
-WHERE Pro_Nom = "quam pede lobortis ligula sit" ORDER BY PPL_PrixUnitaireHTAchat;
+WHERE Pro_Nom = "congue" ORDER BY PPL_PrixUnitaireHTAchat;
 
-SELECT T1.Ent_nom, T1.Ent_NomContact, T1.Ent_Contact, LF_PrixTotalHT
-FROM LivraisonFournisseur NATURAL JOIN ProduitParLivraison NATURAL JOIN (
-	SELECT IDEntreprise, IDProduit, Ent_Nom, Ent_NomContact, Ent_Contact
-	FROM ProduitFournisseur NATURAL JOIN Entreprise NATURAL JOIN Produit
-	WHERE Pro_Nom = "quam pede lobortis ligula sit") AS T1
-WHERE T1.IDProduit = IDProduit;
 
 
 /*
@@ -34,7 +44,7 @@ Combien de commande d'avocat d'espagne ont été livrées durant l'interval d'un
 
 SELECT count(*) as 'Nombre de commandes', SUM(PPC_Quantite) as 'Quantité vendue'
 FROM Commande NATURAL JOIN Produit NATURAL JOIN ProduitParCommande
-WHERE Pro_Nom = "quam pede lobortis ligula sit"
+WHERE Pro_Nom = "congue"
 AND Com_DateLivraison BETWEEN "2014-10-10" AND CURDATE();
 
 
@@ -116,30 +126,6 @@ SELECT Pro_Nom FROM Produit WHERE Produit.IDProduit IN (SELECT ProduitParLivrais
  *
  * TODO
  */
-
-
-/**
- * Dans le cas où quelqu’un est malade à cause de nos produit, on veut savoir quels produits cette personne a acheté sur une date précise.
- * Exemple: jstewartp@un.org nous a écrit pour nous dire qu'il a eu mal au ventre après avoir mangé des produits de la commande qu'il a reçu le 7 Mai 2014.  
- */
-SELECT * FROM Produit WHERE IDProduit IN (SELECT IDProduit FROM ProduitParCommande WHERE IDCommande LIKE (SELECT IDCommande FROM Commande WHERE Commande.IDClient LIKE (SELECT IDClient FROM Client WHERE Cli_Email = 'tperez0@wiley.com') AND Com_DateLivraison='2015-03-18'));
-
-/* un lot pose probleme, nous voulons contacter toutes les personnes qui ont été livrés de ce produit depuis la livraison de ce lot.*/
-
-SELECT Cli_Email, Cli_Prenom, CLiNom
-FROM Client NATURAL JOIN (
-     SELECT IDClient
-     FROM Client NATURAL JOIN Commande
-     WHERE Com_DateCommande => (
-	   SELECT LF_DateArrivee
-	   FROM LivraisonFournisseur NATURAL JOIN ProduitParLivraison
-	   WHERE (IDCategorie, IDProduit) LIKE (
-	   	 SELECT IDCategorie, IDProduit
-		 FROM Produit
-		 WHERE Pro_Nom = "quam pede lobortis ligula sit" ) ) );
-
-
-SELECT Cli_Email, Cli_Prenom, CLi_Nom FROM Client NATURAL JOIN (SELECT IDClient FROM Client NATURAL JOIN Commande WHERE Com_DateLivraison >= (SELECT LF_DateArrivee FROM LivraisonFournisseur NATURAL JOIN ProduitParLivraison WHERE (IDCategorie,IDProduit) IN (SELECT IDCategorie,IDProduit FROM Produit WHERE Pro_Nom = "quam pede lobortis ligula sit" ) AND IDEntreprise IN (SELECT IDEntreprise FROM Entreprise WHERE Ent_Nom = "Flashset"))) as t;
 
 /**
  * Dans le cas où on veut veut constater qu'il y a un pb de cohérence entre le nb de commandes livrées et le nb de produits 
